d04ff6e38360f9631b9667e081866b96a48e4e7e
jebi2420 <jebi2420@gmail.com>
2024-02-13 22:10:04 +0900

    [add] 아이템 추가하기 로직

--- main.js
+++ main.js
@@ -5,3 +5,33 @@
 // 진행 중, 끝남 탭을 누르면, 언더바가 이동한다
 // 끝남 탭은 끝난 아이템만, 진행 중 탭은 진행 중인 아이템만
 // 전체 탭을 누르면 다시 전체 아이템으로 돌아옴
+
+let taskInput = document.getElementById("task-input");
+let btnAdd = document.getElementById("btn-add");
+let taskList = [];
+
+btnAdd.addEventListener("click", addTask)
+
+function addTask(){
+    let taskContent = taskInput.value
+    taskList.push(taskContent)
+    console.log(taskList);
+    render();
+}
+
+function render(){
+    let resultHTML = "";
+    for (let i=0; i<taskList.length; i++){
+        resultHTML += `
+            <div class="task">
+            <span>${taskList[i]}</span>
+            <div class="btns">
+            <button id="check-btn">check</button>
+            <button id="delete-btn">delete</button>
+            </div>
+        </div>        
+        `;
+    }
+
+    document.getElementById("task-board").innerHTML = resultHTML;
+}
